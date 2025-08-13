0390dd84a82ab94b52cd5788af5ab603b93f2d1a
NitiN Sain <nitinsain4811@gmail.com>
2025-08-13 14:53:31 +0530

    'commiting'

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,4 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
-
-export default {
-  build: {
-    rollupOptions: {
-      external: ['react-router-dom']
-    }
-  }
-}
